00349d1d2ae88fe26ac50d1051e88acf1e2ddc19
Guillaume Faguet <guillaume.faguet@protonmail.com>
2023-02-07 20:15:40 +0100

    width and height are now parameters

--- bars.py
+++ bars.py
@@ -7,8 +7,6 @@ from contextlib import contextmanager
 import logging
 
 # ####### PARAMETERS ########### #
-OUT_WIDTH = 600
-OUT_HEIGHT = 800
 number_of_cuts = 5
 # ############################## #
 
@@ -26,6 +24,15 @@ def video_capture(video_path: str) -> Generator[cv2.VideoCapture, None, None]:
         cap.release()
 
 
+def get_video_width_height_frames(
+    video_capture: cv2.VideoCapture,
+) -> tuple[int, int, int]:
+    width = int(video_capture.get(cv2.CAP_PROP_FRAME_WIDTH))
+    height = int(video_capture.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    total_frames = int(video_capture.get(cv2.CAP_PROP_FRAME_COUNT))
+    return width, height, total_frames
+
+
 def show_image(image: cv2.Mat) -> None:
     cv2.imshow("Image", image)
     cv2.waitKey(0)
@@ -35,9 +42,12 @@ def show_image(image: cv2.Mat) -> None:
 def generate_barcode(video_path: Path, output_filename: Path) -> None:
     video_path_str = video_path.as_posix()
     with video_capture(video_path_str) as cap:
-        total_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
-        logging.info(f"Total number of frames: {total_frames}")
+        width, height, total_frames = get_video_width_height_frames(cap)
         nbr_of_captured_frames = np.arange(1, number_of_cuts + 1)
+        logging.info(nbr_of_captured_frames)
+        logging.info(
+            f"Video width: {width}, height: {height}, Nbr frames:{total_frames}"
+        )
         fr_pos = int(
             total_frames / (len(nbr_of_captured_frames) + 2)
         )  # always keep 2 block frames at beginning & end
@@ -45,6 +55,7 @@ def generate_barcode(video_path: Path, output_filename: Path) -> None:
         logging.info(frame_positions_list)
         images = frame_position_to_image(frame_positions_list, cap)
         palette = image_in_column(images)
+        palette = resize_frame(palette, 50)
         show_image(palette)
 
         while question_change():
@@ -54,12 +65,11 @@ def generate_barcode(video_path: Path, output_filename: Path) -> None:
             palette = image_in_column(images)
             show_image(palette)
 
-        avg_cols = ROI_capture_manual(frame_positions_list, cap, nbr_of_captured_frames)
-        concatenated = np.concatenate(avg_cols, axis=1)
-        logging.info(
-            "Resizing {} frames to {}".format(concatenated.shape[1], OUT_WIDTH)
+        avg_cols = ROI_capture_manual(
+            images, cap, nbr_of_captured_frames, width, height
         )
-        barcode = cv2.resize(concatenated, (OUT_WIDTH, OUT_HEIGHT))
+        concatenated = np.concatenate(avg_cols, axis=1)
+        barcode = cv2.resize(concatenated, (width, height))
         cv2.imwrite(output_filename.as_posix(), barcode)
         cv2.destroyAllWindows()
         show_image(barcode)
@@ -73,11 +83,11 @@ def resize_frame(frame_name: cv2.Mat, scale_percent: int) -> cv2.Mat:
 
 
 def frame_position_to_image(
-    frame_positions: list[int], cap: cv2.VideoCapture
+    frame_pos: list[int], cap: cv2.VideoCapture
 ) -> list[cv2.Mat]:
-    """transform a list of frame position into opencv images"""
+    """transform a list of frame positions into opencv images"""
     frame_images = []
-    for i in frame_positions:
+    for i in frame_pos:
         cap.set(cv2.CAP_PROP_POS_FRAMES, i)
         _, frame_image = cap.read()
         frame_images.append(frame_image)
@@ -92,27 +102,28 @@ def image_in_column(images: list[cv2.Mat]) -> cv2.Mat:
 
 
 def modify_frame_list(list: list[int], position: list[int]) -> list[int]:
+    """Change frame numbers by adding 100 to change image captured in the video"""
     for i in position:
         list[i - 1] += 100
     logging.info(list)
     return list
 
 
-def ROI_capture_manual(frame_list, cap: cv2.VideoCapture, nbr_of_captured_frames):
+def ROI_capture_manual(
+    image_list, cap: cv2.VideoCapture, nbr_of_captured_frames, width: int, height: int
+):
     avg_cols = []
-    for i in frame_list:
-        cap.set(cv2.CAP_PROP_POS_FRAMES, i)
-        _, frame = cap.read()
-        frame_height = frame.shape[0]
-        frame = resize_frame(frame, 50)
+    for im in image_list:
+        """decrease image size by 50% to display on screen"""
+        frame = resize_frame(im, 50)
         r = cv2.selectROI(
             windowName="Select ROI", img=frame, showCrosshair=False, fromCenter=False
         )
         output = frame.copy()
         rectangle = cv2.rectangle(
             frame,
-            (r[0] + int(OUT_WIDTH / nbr_of_captured_frames[-1]) - 5, 0),
-            (r[0] + int(OUT_WIDTH / nbr_of_captured_frames[-1]), frame_height),
+            (r[0] + int(width / nbr_of_captured_frames[-1]) - 5, 0),
+            (r[0] + int(width / nbr_of_captured_frames[-1]), height),
             (0, 0, 0),
             -1,
         )
@@ -120,8 +131,8 @@ def ROI_capture_manual(frame_list, cap: cv2.VideoCapture, nbr_of_captured_frames
 
         avg_cols.append(
             output[
-                0:frame_height,
-                r[0] : r[0] + int(OUT_WIDTH / nbr_of_captured_frames[-1]),
+                0:height,
+                r[0] : r[0] + int(width / nbr_of_captured_frames[-1]),
             ]
         )
     return avg_cols
